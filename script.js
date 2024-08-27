626a546e8de96d2e02215c136484b4de3d14a907
jvalenzuelacc <jorge.valenzuela.carcamo@gmail.com>
2024-08-26 21:59:45 -0400

    actualizar script.js

--- script.js
+++ script.js
@@ -1,49 +1,119 @@
-<!DOCTYPE html>
+let entradaTexto = document.querySelector(".entrada-texto");
+let salidaTexto = document.querySelector(".salida-texto");
+let seccionTexto1 = document.querySelector(".texto1");
+let seccionTexto2 = document.querySelector(".texto2");
+let btnCopiar = document.querySelector(".copiar");
 
-<html lang="es">
+function validar(textoValidar){
+    const letras = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","Ñ","O","P","Q","R","S","T","U","V","W","X","Y","Z","Á","É","Í","Ó","Ú","á","é","í","ó","ú"];
+    var conteo = 0;
 
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width">
-        <title>Encriptador de texto</title>
-        <link rel="shortcut icon" href="imagenes/Muñeco.png.png">
-        <link rel="stylesheet" href="reset.css">
-        <link rel="stylesheet" href="style.css">
-        <link rel="preconnect" href="https://fonts.googleapis.com"><link rel="preconnect" href="https://fonts.gstatic.com" crossorigin><link href="https://fonts.googleapis.com/css2?family=Inter&display=swap" rel="stylesheet">
-    </head>
+    for(let posicion = 0; posicion < textoValidar.length; posicion++){
+        for(let letra = 0; letra < letras.length;letra++){
+            if(textoValidar.charAt(posicion) == letras[letra]){
+                conteo++;
+            }
+        }
+    }
+    if(conteo == 0){
+        return true;
+    }
+    return false;
+}
 
-    <body>
-        <header>
-            <img class="logo" src="imagenes/logo-alura.png" alt="Logo de Alura">
-        </header>
+function encriptar() {
+    let texto = entradaTexto.value;
+    let salida = "";
+    if(!validar(texto)){
+        alert("Texto invalido, verifique su texto.")
+        return;
+    }
+    for(let posicion = 0; posicion < texto.length; posicion++){
+        if(texto.charAt(posicion) == "a"){
+            salida = salida + "ai";
+        }
+        else if(texto.charAt(posicion) == "e"){
+            salida = salida + "enter";
+        }
+        else if(texto.charAt(posicion) == "i"){
+            salida = salida + "imes";
+        }
+        else if(texto.charAt(posicion) == "o"){
+            salida = salida + "ober";
+        }
+        else if(texto.charAt(posicion) == "u"){
+            salida = salida + "ufat";
+        }
+        else {
+            salida = salida + texto.charAt(posicion);
+        }
+    }
+    entradaTexto.value = "";
+    salidaTexto.value = salida;
+    ocultar();
+}
 
-        <main>
-            <section>
-                <textarea class="entrada-texto" cols="30" rows="10" placeholder="Ingrese el texto aquí" autofocus></textarea>
-                <div>
-                    <h6 class= "restriccion">&#9888 Solo letras minúsculas y sin acentos.</h6>
-                </div>
-                <div class="botones">
-                    <button name="encriptar" class="encriptar" onclick="encriptar()">Encriptar</button>
-                    <button name="desencriptar" class="desencriptar" onclick="desencriptar()">Desencriptar</button>
-                </div>
-            </section>
-            <section>
-                <textarea class="salida-texto" cols="20" rows="10" disabled></textarea>
-                <div class="dinamico">
-                    <h2 class="texto1">Ningún mensaje fue encontrado</h2>
-                    <h3 class="texto2">Ingresa el texto que desees encriptar o desencriptar.</h3>
-                    <button name="copiar" class="copiar" onclick="copiar()" style="display: none;"><span class="anuncio"></span>Copiar</button>
-                </div>
-            </section>
-        </main>
+function desencriptar() {
+    let texto = entradaTexto.value;
+    var salida = "";
+    if(!validar(texto)){
+        alert("Texto invalido, verifique su texto.")
+        return;
+    }
+    for(let posicion = 0; posicion < texto.length; posicion++){
+        if(texto.charAt(posicion) == "a" && texto.charAt(posicion + 1) == "i"){
+            salida = salida + "a";
+            posicion = posicion + 1;
+        }
+        else if(texto.charAt(posicion) == "e" && texto.charAt(posicion + 1) == "n" && texto.charAt(posicion + 2) == "t" && texto.charAt(posicion + 3) == "e" && texto.charAt(posicion + 4) == "r"){
+            salida = salida + "e";
+            posicion = posicion + 4;
+        }
+        else if(texto.charAt(posicion) == "i" && texto.charAt(posicion + 1) == "m" && texto.charAt(posicion + 2) == "e" && texto.charAt(posicion + 3) == "s"){
+            salida = salida + "i";
+            posicion = posicion + 3;
+        }
+        else if(texto.charAt(posicion) == "o" && texto.charAt(posicion + 1) == "b" && texto.charAt(posicion + 2) == "e" && texto.charAt(posicion + 3) == "r"){
+            salida = salida + "o";
+            posicion = posicion + 3;
+        }
+        else if(texto.charAt(posicion) == "u" && texto.charAt(posicion + 1) == "f" && texto.charAt(posicion + 2) == "a" && texto.charAt(posicion + 3) == "t"){
+            salida = salida + "u";
+            posicion = posicion + 3;
+        }
+        else {
+            salida = salida + texto.charAt(posicion);
+        }
+    }
+    entradaTexto.value = "";
+    salidaTexto.value = salida;
+    ocultar();
+}
 
-        <footer>
-            <p class="copyright">&copy Desarrollado por Jorge Valenzuela</p>
-        </footer>
+function ocultar(){
+    salidaTexto.style.background = "white";
+    seccionTexto1.style.display = "none";
+    seccionTexto2.style.display = "none";
+    btnCopiar.style.display = "";
+}
 
-    </body>
+function mostrar(){
+    salidaTexto.style.background = "#FFF no-repeat center url(imagenes/buscar.png)";
+    seccionTexto1.style.display = "";
+    seccionTexto2.style.display = "";
+    btnCopiar.style.display = "none";
+}
 
-    <script src="script.js"></script>
-
-</html>
+function copiar(){
+    let copia =salidaTexto.value;
+    navigator.clipboard.writeText(copia);
+    
+    let anuncio = document.querySelector(".anuncio");
+    anuncio.textContent = "Texto copiado";
+    anuncio.style.display = "block";
+    setTimeout(() => {
+        anuncio.style.display = "none";
+    }, 950);
+    salidaTexto.value = "";
+    mostrar();
+}
